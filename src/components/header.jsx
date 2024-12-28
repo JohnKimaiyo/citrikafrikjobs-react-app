da43501434dccca7798e00affe80a0d8b53004c9
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-12-28 14:02:53 +0300

    final

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react'
+
+function Header() {
+    return (
+        <div>
+
+            <nav>
+                <Link>
+                    <img src="/logo.png" />
+
+                </Link>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Header
